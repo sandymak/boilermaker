53e6f57f97d97ec2d03d5b029b80b21af52e412e
Sandy Mak <sandy.mak91@gmail.com>
2018-01-09 17:45:33 -0500

    updated webpack config

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const db = require('./server/db/models');
 const app = require('./server');
-const PORT = process.env.PORT || 3000; // this can be useful if you deployt to Heroku
+const PORT = process.env.PORT || 3000; // this can be useful if you deploy to Heroku
+
+// NOTE ==> Production Environment
+// 1) in terminal before running app, type export VARIABLE='secret'
+// 2) to access the variable, type `printenv`
+
 const chalk = require('chalk');
 
 // initializing the app
